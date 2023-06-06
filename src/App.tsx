466dcbe3eeece9bd1c9622966e0891b29f8ec215
mnkhod <mnkhod.dev@gmail.com>
2023-06-06 12:47:50 +0800

    Added new way of using json data

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import data from "../data.json";
+
+const userData = JSON.parse(JSON.stringify(data));
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
@@ -17,7 +20,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <h1>{import.meta.env.VITE_NAME}</h1>
+      <h1>{userData.name}</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -30,7 +33,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
